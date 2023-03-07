66a176a59ca88d041484edd5a823d9b626b7d026
picoxr <pico_devsupport@bytedance.com>
2023-03-07 12:50:27 -0800

    First commit

---
+++ Assets/FTTest.cs
@@ -0,0 +1,137 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.PXR;
+using UnityEngine;
+
+public class FTTest : MonoBehaviour
+{
+    public SkinnedMeshRenderer skin;
+    public SkinnedMeshRenderer tongueBlendShape;
+    public SkinnedMeshRenderer leftEyeExample;
+    public SkinnedMeshRenderer rightEyeExample;
+
+    private UInt64 timestamp;
+    private float[] blendShapeWeight = new float[52];
+    private float[] reserved = new float[16];
+
+    private List<string> blendShapeList = new List<string>
+    {
+"eyeLookDownLeft",
+"noseSneerLeft",
+"eyeLookInLeft",
+"browInnerUp",
+"browDownRight",
+"mouthClose",
+"mouthLowerDownRight",
+"jawOpen",
+"mouthUpperUpRight",
+"mouthShrugUpper",
+"mouthFunnel",
+"eyeLookInRight",
+"eyeLookDownRight",
+"noseSneerRight",
+"mouthRollUpper",
+"jawRight",
+"browDownLeft",
+"mouthShrugLower",
+"mouthRollLower",
+"mouthSmileLeft",
+"mouthPressLeft",
+"mouthSmileRight",
+"mouthPressRight",
+"mouthDimpleRight",
+"mouthLeft",
+"jawForward",
+"eyeSquintLeft",
+"mouthFrownLeft",
+"eyeBlinkLeft",
+"cheekSquintLeft",
+"browOuterUpLeft",
+"eyeLookUpLeft",
+"jawLeft",
+"mouthStretchLeft",
+"mouthPucker",
+"eyeLookUpRight",
+"browOuterUpRight",
+"cheekSquintRight",
+"eyeBlinkRight",
+"mouthUpperUpLeft",
+"mouthFrownRight",
+"eyeSquintRight",
+"mouthStretchRight",
+"cheekPuff",
+"eyeLookOutLeft",
+"eyeLookOutRight",
+"eyeWideRight",
+"eyeWideLeft",
+"mouthRight",
+"mouthDimpleLeft",
+"mouthLowerDownLeft",
+"tongueOut",
+    };
+
+    private int[] indexList = new int[52];
+    private int tongueIndex;
+    private int leftLookDownIndex;
+    private int leftLookUpIndex;
+    private int leftLookInIndex;
+    private int leftLookOutIndex;
+
+    private int rightLookDownIndex;
+    private int rightLookUpIndex;
+    private int rightLookInIndex;
+    private int rightLookOutIndex;
+
+    private PxrFaceTrackingInfo faceTrackingInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < indexList.Length; i++)
+        {
+            indexList[i] = skin.sharedMesh.GetBlendShapeIndex(blendShapeList[i]);
+        }
+        tongueIndex = tongueBlendShape.sharedMesh.GetBlendShapeIndex("tongueOut");
+        leftLookDownIndex = leftEyeExample.sharedMesh.GetBlendShapeIndex("eyeLookDownLeft");
+        leftLookUpIndex = leftEyeExample.sharedMesh.GetBlendShapeIndex("eyeLookUpLeft");
+        leftLookInIndex = leftEyeExample.sharedMesh.GetBlendShapeIndex("eyeLookInLeft");
+        leftLookOutIndex = leftEyeExample.sharedMesh.GetBlendShapeIndex("eyeLookOutLeft");
+        rightLookDownIndex = rightEyeExample.sharedMesh.GetBlendShapeIndex("eyeLookDownRight");
+        rightLookUpIndex = rightEyeExample.sharedMesh.GetBlendShapeIndex("eyeLookUpRight");
+        rightLookInIndex = rightEyeExample.sharedMesh.GetBlendShapeIndex("eyeLookInRight");
+        rightLookOutIndex = rightEyeExample.sharedMesh.GetBlendShapeIndex("eyeLookOutRight");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (PXR_Plugin.System.UPxr_QueryDeviceAbilities(PxrDeviceAbilities.PxrTrackingModeFaceBit))
+        {
+            //PXR_System.GetFaceTrackingData(0, , ref timestamp, ref blendShapeWeight, ref reserved);
+            PXR_System.GetFaceTrackingData(0, GetDataType.PXR_GET_FACE_DATA, ref faceTrackingInfo);
+            blendShapeWeight = faceTrackingInfo.blendShapeWeight;
+            float[] data = blendShapeWeight;
+            for (int i = 0; i < data.Length; ++i)
+            {
+                //Debug.Log("current blendshape[" + skin.sharedMesh.GetBlendShapeName(i) + "]: " + 100 * data[i]);
+                if (indexList[i] >= 0)
+                {
+                    skin.SetBlendShapeWeight(indexList[i], 100 * data[i]);
+                }
+            }
+            tongueBlendShape.SetBlendShapeWeight(tongueIndex, 100 * data[51]);
+            //Debug.Log("Tongue Value:" + data[51].ToString());
+            leftEyeExample.SetBlendShapeWeight(leftLookUpIndex, 100 * data[31]);
+            leftEyeExample.SetBlendShapeWeight(leftLookDownIndex, 100 * data[0]);
+            leftEyeExample.SetBlendShapeWeight(leftLookInIndex, 100 * data[2]);
+            leftEyeExample.SetBlendShapeWeight(leftLookOutIndex, 100 * data[44]);
+            rightEyeExample.SetBlendShapeWeight(rightLookUpIndex, 100 * data[35]);
+            //Debug.Log("RightEyeLookUp Value:"+ data[35].ToString());
+            rightEyeExample.SetBlendShapeWeight(rightLookDownIndex, 100 * data[12]);
+            //Debug.Log("RightEyeLookDown Value:" + data[12].ToString());
+            rightEyeExample.SetBlendShapeWeight(rightLookInIndex, 100 * data[11]);
+            rightEyeExample.SetBlendShapeWeight(rightLookOutIndex, 100 * data[45]);
+        }
+    }
+}
+
